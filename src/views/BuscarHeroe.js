0e1c36c67701508ea6dd21cd4157b387fd6d99e8
CiraCas <zyra_cas@hotmail.com>
2021-11-04 12:13:36 +0100

    Tabla hecha y código limpio

--- src/views/BuscarHeroe.js
+++ src/views/BuscarHeroe.js
@@ -1,66 +1,44 @@
 import React, {useEffect, useState} from 'react';
 import Heroe from '../components/Heroe';
-import axios from 'axios';
-
+import {searchCall} from "../utils/calls"; 
 
 const BuscarHeroe = () => {
-    const [letra, setLetra] = useState('a');
-    let url = `https://gateway.marvel.com/v1/public/characters?nameStartsWith=${letra}&apikey=bcb5cdc12116ddeb6a2b6a25d072a121&ts=1234&hash=a1a6cbfa9fb2c207fa141966f0150269`;
-    const [heroes, setHeroes] = useState([]);
+  
+  const [letra, setLetra] = useState('a');
+  let url = `https://gateway.marvel.com/v1/public/characters?nameStartsWith=${letra}&apikey=bcb5cdc12116ddeb6a2b6a25d072a121&ts=1234&hash=a1a6cbfa9fb2c207fa141966f0150269`;
+  const [heroes, setHeroes] = useState([]);
 
-    const buscarHeroe = (e) => {
-      setLetra(
-        e.target.value
-      )  
-    }
+  const buscarHeroe = (e) => {
+    setLetra(
+      e.target.value
+    )  
+  }
 
-    const llamada = ()=>{
-      axios.get(url)
-      .then(
-        result => {
-          setHeroes(result.data.data.results);
-        }
-      )
-      .catch(console.log)
-    }
+  useEffect(() => {
+    searchCall(url).then(
+      result => {
+        setHeroes(result.data.data.results)
+      }
+    ).catch(console.log);
+  }, [letra, url])
 
-    useEffect(() => {
-      llamada()
-    }, [letra])
-
-    //mostrar heroes
-      /* const mostrarHeroes = () =>{
-        const arrayForEachHeroe = [];
-        //debugger
-        heroes.forEach((heroe, i)=>{
-          arrayForEachHeroe.push(
-            <div className="grupo-foto" key={i}>
-              <h2>{heroe.name}</h2>
-              <img src={heroe.thumbnail.path+"/portrait_xlarge.jpg"} alt={heroe.name}/>
-              <h3>{heroe.id}</h3>
-              <p>{heroe.description}</p>
-            </div>
-          )
-        }) 
-        return(arrayForEachHeroe)
-      } */
-    return ( 
-        <>
-            <input
-                onChange={buscarHeroe}
-                placeholder="Busca Heroe"
-            />
-            {/* <div className="container grupos-fotos">{mostrarHeroes()}</div> */}
-            <div className="container grupos-fotos">
-              {heroes.map(heroe=>(
-                <Heroe
-                  key = {heroe.id}
-                  heroe = {heroe}
-                />
-              ))}
-          </div>
-        </> 
-    );
+  return ( 
+    <>
+      <input
+        onChange={buscarHeroe}
+        placeholder="Busca Heroe"
+      />
+        
+      <div className="container grupos-fotos">
+        {heroes.map(heroe=>(
+          <Heroe
+            key = {heroe.id}
+            heroe = {heroe}
+          />
+        ))}
+      </div>
+    </> 
+  );
 }
  
 export default BuscarHeroe;
