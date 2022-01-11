ef40de1033bd6d68542a78fcc0c0e1886f4acb6e
CiraCas <zyra_cas@hotmail.com>
2022-01-11 12:04:04 +0100

    Delete heroes

--- src/components/Heroe.js
+++ src/components/Heroe.js
@@ -1,17 +1,41 @@
+import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
+import { deleteCall } from "../utils/calls";
 
 const Heroe = ({heroe}) => {
 
     const history = useHistory();
+    const { id } = heroe;
+    const [eliminar, setEliminar] = useState(false)
+    console.log(eliminar)
+
+    useEffect(() => {
+        
+        if(eliminar === true){
+            deleteCall(id).then(
+                result => {
+                    console.log(result);
+                    console.log(result.data);
+                }
+                ).catch(console.log); 
+            console.log('borrar'+ id)
+            window.alert("Heroe Eliminado");
+            //history.push('/buscarheroe')
+        }else{
+            console.log('no borrar'+id)
+        }
+        //setEliminar(false);
+          
+
+    }, [eliminar])
 
     const borrar = () => {
-        console.log('borrar '+ heroe.id)
+        setEliminar(true)
+       
     }
 
     const modificar = id => {
-        //history.push(`/modifheroe?id=${id}`);
         history.push(`/modifheroe/${id}`);
-        
     }
     return ( 
         <div className="grupo-foto" key={heroe.id}>
@@ -30,7 +54,7 @@ const Heroe = ({heroe}) => {
                 <button 
                     type="button"
                     className="boton"
-                    onClick={() => modificar (heroe.id)}
+                    onClick={() => modificar (id)}
                 >
                     Modificar
                 </button>
--- src/views/ModifHeroe.js
+++ src/views/ModifHeroe.js
@@ -79,6 +79,7 @@ const ModifHeroe = () => {
               ).catch(console.log);
 
             setControlCambio(false);
+            history.push('/buscarheroe')
         }
         
       }, [heroe]) 
